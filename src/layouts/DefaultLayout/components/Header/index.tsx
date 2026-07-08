bf82ca9483d02caa129d7b32c90ca207ef19ecd2
Cloly <trongbut03@gmail.com>
2026-07-08 09:46:14 +0700

    feat(account-menu): ZTRUYEN-3 sync dropdown avatar sidebar/header and add profile page

--- src/layouts/DefaultLayout/components/Header/index.tsx
+++ src/layouts/DefaultLayout/components/Header/index.tsx
@@ -1,18 +1,6 @@
-// ** React router
-import {Link} from "react-router-dom";
-
 // ** Shadcn ui
 import {SidebarTrigger} from "@/components/ui/sidebar.tsx";
 import {Separator} from "@/components/ui/separator.tsx";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 // ** Layout component
 import Search from "@/layouts/DefaultLayout/components/Search";
@@ -21,9 +9,7 @@ import Panel from "@/layouts/DefaultLayout/components/Panel";
 // ** Components
 import {ModeToggle} from "@/components/common/ModeToggle";
 import AvatarWithFrame from "@/components/common/AvatarWithFrame";
-
-// ** Icons
-import {BadgeCheck, Bell, LogOut} from "lucide-react";
+import AccountDropdown from "@/components/common/AccountDropdown";
 
 import {useAuth} from "@/hooks/common/useAuth.ts";
 
@@ -52,61 +38,16 @@ const Header = () => {
                         orientation="vertical"
                         className="data-[orientation=vertical]:h-4 mx-2"
                     />
-                    <DropdownMenu>
-                        <DropdownMenuTrigger>
+                    <AccountDropdown>
+                        <button className="cursor-pointer focus:outline-none flex items-center justify-center">
                             <AvatarWithFrame
                                 avatarUrl={user?.avatar?.url}
                                 avatarName={user?.name}
                                 frameUrl={user?.avatar_frame?.image?.url}
                                 frameName={user?.avatar_frame?.name}
                             />
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent
-                            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-                            align='end'
-                        >
-                            <DropdownMenuGroup>
-                                <DropdownMenuLabel>
-                                    <div className='flex items-center gap-2 text-left text-sm'>
-                                        <AvatarWithFrame
-                                            className='size-6 ml-1 mr-2'
-                                            avatarUrl={user?.avatar?.url}
-                                            avatarName={user?.name}
-                                            frameUrl={user?.avatar_frame?.image?.url}
-                                            frameName={user?.avatar_frame?.name}
-                                        />
-                                        <div className='grid flex-1 text-left text-sm leading-tight'>
-                                            <span className='truncate font-semibold'>{user?.name}</span>
-                                            <span
-                                                className='text-muted-foreground truncate text-xs'>{user?.email}</span>
-                                        </div>
-                                    </div>
-                                </DropdownMenuLabel>
-                                <DropdownMenuSeparator/>
-                                <DropdownMenuGroup>
-                                    <Link to='/'>
-                                        <DropdownMenuItem>
-                                            <BadgeCheck/>
-                                            Tài khoản
-                                        </DropdownMenuItem>
-                                    </Link>
-                                    <Link to='/'>
-                                        <DropdownMenuItem>
-                                            <Bell/>
-                                            Thông báo
-                                        </DropdownMenuItem>
-                                    </Link>
-                                </DropdownMenuGroup>
-                                <DropdownMenuSeparator/>
-                                <Link to='/'>
-                                    <DropdownMenuItem>
-                                        <LogOut className='text-red-500'/>
-                                        <span className='text-red-500 hover:text-red-500'>Đăng xuất</span>
-                                    </DropdownMenuItem>
-                                </Link>
-                            </DropdownMenuGroup>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                        </button>
+                    </AccountDropdown>
                 </div>
             </div>
         </header>
